48a87b0dcbb7932bc7b1f8df91e62e0e372cd789
soljatuulia <soljatuulia@gmail.com>
2024-03-16 13:16:43 +0200

    add api request for single photo

--- api/photos.ts
+++ api/photos.ts
@@ -1,7 +1,19 @@
-export const getPhotos = async (page: number = 1, limit: number = 10) => {
+export const getPhotos = async (page: number = 1, limit: number = 12) => {
   const response = await fetch(`http://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`);
   if (!response.ok) {
     throw new Error('Failed to fetch photos');
   }
-  return await response.json();
+  const totalCount = parseInt(response.headers.get('X-Total-Count') || '0', 10);
+  const totalPages = Math.ceil(totalCount / limit);
+  const photos = await response.json();
+  return { photos, totalPages };
+};
+
+export const getPhotoById = async (id: number) => {
+  const response = await fetch(`http://jsonplaceholder.typicode.com/photos/${id}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch photo');
+  }
+  const photo = await response.json();
+  return photo;
 };
