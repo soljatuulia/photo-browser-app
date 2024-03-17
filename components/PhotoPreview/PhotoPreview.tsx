06cce4bd2d0d6d6885e638e8d37b8bffe7a44e2d
soljatuulia <soljatuulia@gmail.com>
2024-03-17 15:43:08 +0200

    enhance error handling

--- components/PhotoPreview/PhotoPreview.tsx
+++ components/PhotoPreview/PhotoPreview.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { SimpleGrid, Stack, Title } from '@mantine/core';
+import { SimpleGrid, Stack, Text, Title } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { Photo } from '../../types/photo';
 import type { Album } from '@/types/album';
@@ -13,6 +13,10 @@ interface AlbumProps {
 }
 
 export default function Album({ photos, album }: AlbumProps) {
+  if (!album) {
+    return <Text>Sorry! No album data to display.</Text>;
+  }
+
   const isMobile = useMediaQuery('(max-width: 768px)');
   const isTablet = useMediaQuery('(min-width: 769px) and (max-width: 1024px)');
 
@@ -21,20 +25,28 @@ export default function Album({ photos, album }: AlbumProps) {
   return (
     <Stack maw={500}>
       <Title order={3} lineClamp={1} className={classes.title}>
-        {album.title}
+        {album.title || 'Untitled'}
       </Title>
-      <SimpleGrid cols={cols} spacing={15} className={classes.grid}>
-        {photos.map((photo) => (
-          <div key={photo.id}>
-            <Link href={`/photos/${photo.id}`}>
-              <Image src={photo.thumbnailUrl} alt={photo.title} width={150} height={150} />
-            </Link>
-          </div>
-        ))}
-      </SimpleGrid>
-      <Link href={`/albums/${album.id}`} className={classes.more}>
-        View more...
-      </Link>
+      {photos.length > 0 ? (
+        <>
+          <SimpleGrid cols={cols} spacing={15} className={classes.grid}>
+            {photos.map((photo) =>
+              photo && photo.id && photo.thumbnailUrl && photo.title ? (
+                <div key={photo.id}>
+                  <Link href={`/photos/${photo.id}`}>
+                    <Image src={photo.thumbnailUrl} alt={photo.title} width={150} height={150} />
+                  </Link>
+                </div>
+              ) : null
+            )}
+          </SimpleGrid>
+          <Link href={`/albums/${album.id}`} className={classes.more}>
+            View more...
+          </Link>
+        </>
+      ) : (
+        <Text>Sorry, no photos found.</Text>
+      )}
     </Stack>
   );
 }
