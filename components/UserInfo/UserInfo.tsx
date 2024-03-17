570717fd568c6548a26def9f30673d1f65c91ba4
soljatuulia <soljatuulia@gmail.com>
2024-03-17 16:02:42 +0200

    enhance error handling

--- components/UserInfo/UserInfo.tsx
+++ components/UserInfo/UserInfo.tsx
@@ -3,16 +3,24 @@ import { Title } from '@mantine/core';
 import classes from './UserInfo.module.css';
 
 interface UserInfoProps {
-  totalPhotos: number;
-  totalAlbums: number;
+	totalPhotos: number | null | undefined;
+	totalAlbums: number | null | undefined;
 }
 
 export default function UserInfo({ totalPhotos, totalAlbums }: UserInfoProps) {
-  return (
-    <div className={classes.container}>
-      <Title order={4}>Fun facts</Title>
-      <p>Total Albums: {totalAlbums}</p>
-      <p>Total Photos: {totalPhotos}</p>
-    </div>
-  );
+	if (totalPhotos === null || totalPhotos === undefined || typeof totalPhotos !== 'number') {
+		totalPhotos = 0;
+	}
+
+	if (totalAlbums === null || totalAlbums === undefined || typeof totalAlbums !== 'number') {
+		totalAlbums = 0;
+	}
+
+	return (
+		<div className={classes.container}>
+			<Title order={4}>Fun facts about user</Title>
+			<p>Total Albums: {totalAlbums}</p>
+			<p>Total Photos: {totalPhotos}</p>
+		</div>
+	);
 }
