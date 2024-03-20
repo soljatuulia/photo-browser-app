580b8e2fe3c1f43c0f6a935c39948f308a55407e
soljatuulia <soljatuulia@gmail.com>
2024-03-20 20:17:10 +0200

    add config for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,15 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
+/** @type {import('next').NextConfig} */
+export const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  basePath: '/photo-browser-app',
+};
+
 export default withBundleAnalyzer({
-  reactStrictMode: false,
+  ...nextConfig,
   eslint: {
     ignoreDuringBuilds: true,
   },
